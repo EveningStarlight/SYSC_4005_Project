a085f436fbaab753f7f3e4bf821b60000c29e1cd
Evening Starlight <55841241+EveningStarlight@users.noreply.github.com>
2022-02-24 22:24:30 -0500

    add arg for changing seed

--- simulation/main.py
+++ simulation/main.py
@@ -1,8 +1,11 @@
 import simulation
+import sys
 
 if __name__ == '__main__':
     SIMULATION_RUN_TIME = 8*60
-    seed = 1234
+    print("length: " + str(len(sys.argv)))
+    print("argv: " + str(sys.argv))
+    seed = sys.argv[1] if len(sys.argv) > 1 else 1234
 
     sim = simulation.Simulation(SIMULATION_RUN_TIME, seed)
 
