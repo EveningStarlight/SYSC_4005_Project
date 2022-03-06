d1af339f93384db1e85c0ed716dafcf75932985c
Evening Starlight <55841241+EveningStarlight@users.noreply.github.com>
2022-03-06 14:37:13 -0500

    allow runs of sequential simulations (#11)

--- simulation/main.py
+++ simulation/main.py
@@ -5,8 +5,9 @@ if __name__ == '__main__':
     SIMULATION_RUN_TIME = 8*60
     print("length: " + str(len(sys.argv)))
     print("argv: " + str(sys.argv))
-    seed = sys.argv[1] if len(sys.argv) > 1 else 1234
+    seed = int(sys.argv[1]) if len(sys.argv) > 1 else 1234
+    numberOfSims = int(sys.argv[2]) if len(sys.argv) > 2 else 1
 
-    sim = simulation.Simulation(SIMULATION_RUN_TIME, seed)
-
-    sim.start()
+    for i in range(numberOfSims):
+        sim = simulation.Simulation(SIMULATION_RUN_TIME, seed+i)
+        sim.start()
