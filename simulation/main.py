82ba0fe5518757801e1263bfa052d1b2d9f13976
Evening Starlight <55841241+EveningStarlight@users.noreply.github.com>
2022-03-28 17:18:51 -0400

    remove arg prints

--- simulation/main.py
+++ simulation/main.py
@@ -3,8 +3,6 @@ import sys
 
 if __name__ == '__main__':
     SIMULATION_RUN_TIME = 8*60
-    print("length: " + str(len(sys.argv)))
-    print("argv: " + str(sys.argv))
     seed = int(sys.argv[1]) if len(sys.argv) > 1 else 1234
     numberOfSims = int(sys.argv[2]) if len(sys.argv) > 2 else 1
 
